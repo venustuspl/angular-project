33dbafbd157bf7d94cd63d1d38ca8b6dd7581078
venustuspl <venustus.pl@gmail.com>
2024-09-17 08:25:53 +0200

    files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { TodoListComponent } from './todo-list/todo-list.component';
+import { FooterComponent } from './footer/footer.component';
+import { AlertComponent } from './shared/components/alert/alert.component';
+import { AddTodoFormComponent } from './todo-list/add-todo-form/add-todo-form.component';
+import { TodoComponent } from './todo-list/todo/todo.component';
+import { ModalComponent } from './shared/components/modal/modal.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    TodoListComponent,
+    FooterComponent,
+    AlertComponent,
+    AddTodoFormComponent,
+    TodoComponent,
+    ModalComponent
   ],
   imports: [
     BrowserModule
