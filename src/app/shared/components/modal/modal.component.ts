67e73e87f8d082c8b7d6c1d385811bc43d3fd15a
tom <venustus.pl@gmail.com>
2024-11-04 08:10:49 +0100

    funkcja defer (promise eager(to lazy -defer) | observable lazy)

--- src/app/shared/components/modal/modal.component.ts
+++ src/app/shared/components/modal/modal.component.ts
@@ -1,37 +1,21 @@
 import {
   Component,
   EventEmitter,
-  Input, OnDestroy, OnInit,
+  Input,
   Output
 } from '@angular/core';
-import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-modal',
   templateUrl: './modal.component.html',
   styleUrls: ['./modal.component.css']
 })
-export class ModalComponent implements OnInit, OnDestroy{
+export class ModalComponent{
   @Input() title!: string;
   @Output() close = new EventEmitter<void>();
-  sub!: Subscription;
 
 
   onClose() {
     this.close.emit();
-  }
-  ngOnInit(): void {
-    // this.sub = of(1, 2, 3).subscribe({
-    //   next: value => console.log(value),
-    //   error: err => console.log(err),
-    //   complete: () => console.log('Test')
-    // })
-  }
-
-  ngOnDestroy(): void {
-    if (this.sub) {
-      // this.sub.unsubscribe();
-    }
-    // console.log(this.sub)
   }
 }
